e13ef6f3a6e160acd7331b06983705ed5f7d2b4c
LoganUnited <148744719+LoganUnited@users.noreply.github.com>
2025-06-26 19:21:59 +0300

    Delete GameTelegram directory

--- GameTelegram/game/world/locations/las_venturas.py
+++
@@ -1,7 +0,0 @@
-from game.world.locations.base import BaseLocation
-
-class LasVenturas(BaseLocation):
-    def __init__(self):
-        super().__init__()
-        self.name = "Лас-Вентурас"
-        self.description = "Город азарта и казино"
--- GameTelegram/game/world/locations/san_fierro.py
+++
@@ -1,7 +0,0 @@
-from game.world.locations.base import BaseLocation
-
-class SanFierro(BaseLocation):
-    def __init__(self):
-        super().__init__()
-        self.name = "Сан-Фиерро"
-        self.description = "Портовый город с холмами"
